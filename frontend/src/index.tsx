278f8eeeac652b9a03c052330fd8690ae53da7a0
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-11-03 14:45:28 +0000

    remove styles

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,13 @@
-import { ApolloProvider } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ApolloProvider } from '@apollo/client'
 import { client } from './lib/apollo'
-import GlobalStyles from "./Styles/GlobalStyles"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyles>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
-    </GlobalStyles>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
