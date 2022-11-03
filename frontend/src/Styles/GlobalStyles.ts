1c902ac7ca00f5baae18b29d7e67490d41cdb921
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-11-03 12:22:40 +0000

    set up style-component

---
+++ frontend/src/Styles/GlobalStyles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: teal;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+  }
+`;
+
+export default GlobalStyles;
