12971cef997f2fb5f963588153fb74f1b14036dc
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-11-03 12:21:03 +0000

    set up apollo client

---
+++ frontend/src/lib/apollo.ts
@@ -0,0 +1,13 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+  HttpLink,
+} from "@apollo/client";
+import fetch from "cross-fetch";
+
+export const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:4000/", fetch }),
+  cache: new InMemoryCache(),
+});
