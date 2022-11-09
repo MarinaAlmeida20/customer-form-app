32655047199ad244812e39bf93289c02208d042a
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-11-09 18:12:40 +0000

    config

--- backend/index.ts
+++ backend/index.ts
@@ -8,22 +8,6 @@ import { buildSchema } from "type-graphql";
 import { ApolloServer } from "apollo-server";
 import { ClientResolver } from "./Resolvers/ClientResolver";
 
-// async function main() {
-//   const schema = await buildSchema({
-//     resolvers: [ClientResolver],
-//     emitSchemaFile: path.resolve(__dirname, "schema.ggl"),
-//   });
-
-//   const server = new ApolloServer({
-//     schema,
-//   });
-
-//   const { url } = await server.listen();
-//   console.log(`🚀  Server ready at: ${url}`);
-// }
-
-// main();
-
 async function main() {
   const schema = await buildSchema({
     resolvers: [ClientResolver],
