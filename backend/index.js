b8cfbf8e729106631769943d25bc0848e4f07af1
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-11-09 18:32:18 +0000

    config

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import "reflect-metadata";
+import path from "path";
+
+require("dotenv").config({ path: ".env" });
+import "./mongodb/connect";
+
+import { buildSchema } from "type-graphql";
+import { ApolloServer } from "apollo-server";
+import { ClientResolver } from "./Resolvers/ClientResolver";
+
+async function main() {
+  const schema = await buildSchema({
+    resolvers: [ClientResolver],
+    emitSchemaFile: path.resolve(__dirname, "schema.ggl"),
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
+  console.log(`
+    🚀  Server is running!
+    🔉  Listening on port ${port}
+    📭  Query at ${url}
+  `);
+}
+main();
