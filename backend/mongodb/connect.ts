a9fe62e2eca8befb6ca2a4029d6989ae7cc783f1
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-11-11 18:44:51 +0000

    config

--- backend/mongodb/connect.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose
-  .connect(process.env.MONGODB_URI as string)
-  .then(() => console.log("Mongodb Connected"))
-  .catch((error) => console.log(error));
